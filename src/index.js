cd230da907cd1f7c84bf1787b8f6e54e970f0073
issy2500 <66333080+issy2500@users.noreply.github.com>
2021-02-13 17:47:52 -0500

    added Books to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//stateless function component or dump component
+//always return JSX
+//setup vars
+
+const firstBook ={
+  img:"https://images-na.ssl-images-amazon.com/images/I/81WuEQjTE1L._AC_UL200_SR200,200_.jpg",
+  title: "The Princess Spy: The True Story of World War II Spy Aline Griffith, Countess of Romanones",
+  author: "Larry Loftis",
+}
+const secondBook ={
+  img:"https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg",
+  title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones ",
+  author: "James Clear",
+}
+function BookList(){
+  return (
+ <section className="booklist">
+   <Book img={firstBook.img}  title ={firstBook.title} author={firstBook.author}/>
+    <Book img={secondBook.img} title ={secondBook.title} author={secondBook.author} >
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore cum animi voluptas
+        temporibus quo delectus pariatur, hic voluptatum accusantium illo.
+      </p>
+</Book>
+      
+    <Book/>
+    <Book/>
+ </section>
+  )
+}
+
+const Book = (props) =>{
+
+const {img,title,author, children}=props;
+  return (<article className="book">
+    <img src={img} alt=""/>
+    <h4> {title} </h4>
+    <h3> {author} </h3>
+    {props.children}
+  </article>
+  );
+};
+
+ReactDom.render(<BookList/>,document.getElementById('root'));
+
+
+
