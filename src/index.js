87c5a519e0eeb83340b63105ef76d0b2b94cd7a2
issy2500 <66333080+issy2500@users.noreply.github.com>
2021-02-14 11:40:22 -0500

    added spread operator to BookList function

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ import "./index.css";
 //setup vars
 
 const books =[
-  {
+  { id:1,
   img:"https://images-na.ssl-images-amazon.com/images/I/81WuEQjTE1L._AC_UL200_SR200,200_.jpg",
   title: "The Princess Spy: The True Story of World War II Spy Aline Griffith, Countess of Romanones",
   author: "Larry Loftis",
 },
-{
+{id:2,
   img:"https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg",
   title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones ",
   author: "James Clear",
 },
-{
+{id:3,
   img:"https://images-na.ssl-images-amazon.com/images/I/41bChkOa4zL._AC_UL200_SR200,200_.jpg",
   title: "Think Again: The Power of Knowing What You Don't Know",
   author: "Adam Grant",
@@ -30,10 +30,10 @@ const books =[
 
 function BookList(){
   return <section className="booklist">
-   {books.map((book)=>{
+   {books.map((book,index)=>{
      const {img,title,author}= book;
  return (
-   <Book book={book}>
+   <Book key={book.index} {...book}>
 </Book>
    
  );
@@ -43,7 +43,7 @@ function BookList(){
 
 const Book = (props) =>{
 
-const {img,title,author }=props.book;
+const {img,title,author }=props;
  
 return (
   <article className="book">
